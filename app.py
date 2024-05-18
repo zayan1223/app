0f3c6e41953da450622eae7ae391690418e3b48d
zayan1223 <125382216+zayan1223@users.noreply.github.com>
2024-05-18 22:43:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -332,4 +332,5 @@ print(job_titles)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
